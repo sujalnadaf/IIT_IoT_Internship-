01522d9698962b08d0548e5e86281431bac52f83
sujalnadaf <sujalnadaf@gmail.com>
2025-12-24 23:31:43 -0800

    Assignment 9

---
+++ Assignment 9/MQ2.ino.ino
@@ -0,0 +1,23 @@
+/*
+ * Created by ArduinoGetStarted.com
+ *
+ * This example code is in the public domain
+ *
+ * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-gas-sensor
+ */
+
+#define AO_PIN 2  
+
+void setup() {
+  // initialize serial communication
+  Serial.begin(115200);
+  Serial.println("Warming up the MQ2 sensor");
+  delay(20000);  // wait for the MQ2 to warm up
+}
+
+void loop() {
+  int gasValue = analogRead(AO_PIN);
+
+  Serial.print("MQ2 sensor AO value: ");
+  Serial.println(gasValue);
+}
